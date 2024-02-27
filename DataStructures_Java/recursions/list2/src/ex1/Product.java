6633820a0576af228a4b074cc40e881db584292f
Fábio Marane <84942346+maranefabio@users.noreply.github.com>
2024-02-26 22:08:14 -0300

    Files refactored.

---
+++ DataStructures_Java/recursions/list2/src/ex1/Product.java
@@ -0,0 +1,34 @@
+package ex1;
+
+import javax.swing.JOptionPane;
+
+public class Product {
+
+    public Product() {
+        super();
+    }
+
+    public int recursion(int A, int B) {
+        if(A==0) {
+            return 0;
+        }
+        return B + recursion(A - 1, B);
+    }
+
+    public void calculate() {
+        int A = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                        "First term: "
+                )
+        );
+        int B = Integer.parseInt(
+                JOptionPane.showInputDialog(
+                        "Second term: "
+                )
+        );
+
+        JOptionPane.showMessageDialog(
+                null, A + " + " + B + " = " + recursion(A, B)
+        );
+    }
+}
---
+++ DataStructures_Java/recursions/list2/src/user_interface/UserInterface.java
@@ -0,0 +1,13 @@
+package user_interface;
+
+import ex1.Product;
+import ex2.Division;
+import ex3.EvenInVector;
+import ex4.DoubleFactorial;
+
+public class UserInterface {
+    public static void main(String[] args) {
+        Product product = new Product();
+        product.calculate();
+    }
+}
