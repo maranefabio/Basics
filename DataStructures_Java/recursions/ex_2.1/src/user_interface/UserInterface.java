23704fdd308d816b394961fb7491892e05115397
Fábio Marane <84942346+maranefabio@users.noreply.github.com>
2024-02-26 22:03:00 -0300

    Recursion list 2 added.

---
+++ DataStructures_Java/recursions/ex_2.1/src/user_interface/UserInterface.java
@@ -0,0 +1,10 @@
+package user_interface;
+
+import ex1.Product;
+import ex2.Division;
+import ex3.EvenInVector;
+import ex4.DoubleFactorial;
+public class UserInterface {
+    public static void main(String[] args) {
+    }
+}
